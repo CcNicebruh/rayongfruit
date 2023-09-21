b1f92967a954a388c01c72bc424d847fe78ec3e8
NiceKun <fortimmy@hotmail.com>
2023-09-21 12:41:23 +0700

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'rayongfruit.nicekun.dev',
-  title: 'คิดไม่ออก',
+  title: 'ผลไม้ระยอง',
   subtitle: 'เว็บไซต์รวมผลไม้ในเมีองระยอง',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
