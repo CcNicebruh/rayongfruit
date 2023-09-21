fb0da93727afd1e7c55630554119b93d77cc2a69
Chanakan Prayoonphong <fortimmy@hotmail.com>
2023-09-21 11:23:37 +0700

    new changes

---
+++ src/lib/config/general.ts
@@ -0,0 +1,96 @@
+import type { ThemeConfig, HeadConfig, HeaderConfig, FooterConfig, DateConfig, FeedConfig } from '$lib/types/general'
+
+export const theme: ThemeConfig = [
+  {
+    name: 'cmyk',
+    text: '🖨 Light'
+  },
+  {
+    name: 'dracula',
+    text: '🧛 Dark'
+  },
+  {
+    name: 'valentine',
+    text: '🌸 Valentine'
+  },
+  {
+    name: 'aqua',
+    text: '💦 Aqua'
+  },
+  {
+    name: 'synthwave',
+    text: '🌃 Synthwave'
+  },
+  {
+    name: 'night',
+    text: '🌃 Night'
+  },
+  {
+    name: 'lofi',
+    text: '🎶 Lo-Fi'
+  },
+  {
+    name: 'lemonade',
+    text: '🍋 Lemonade'
+  },
+  {
+    name: 'cupcake',
+    text: '🧁 Cupcake'
+  },
+  {
+    name: 'garden',
+    text: '🏡 Garden'
+  },
+  {
+    name: 'retro',
+    text: '🌇 Retro'
+  },
+  {
+    name: 'black',
+    text: '🖤 Black'
+  }
+]
+
+export const head: HeadConfig = {}
+
+export const header: HeaderConfig = {
+  nav: [
+    {
+      text: 'กินผักและผลไม้ตามฤดูกาล',
+      link: '/svaf'
+    },
+    {
+      text: 'ผลไม้ (ทุเรียน)',
+      link: '/tulean'
+    },
+    {
+      text: 'การปลูกมังคุด',
+      link: '/mangkut'
+    }
+  ]
+}
+
+export const footer: FooterConfig = {
+  nav: [
+    {
+      text: 'Feed',
+      link: '/atom.xml'
+    },
+    {
+      text: 'Sitemap',
+      link: '/sitemap.xml'
+    }
+  ]
+}
+
+export const date: DateConfig = {
+  locales: 'en-US',
+  options: {
+    year: '2-digit',
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric'
+  }
+}
+
+export const feed: FeedConfig = {}
---
+++ src/lib/config/site.ts
@@ -0,0 +1,17 @@
+import type { SiteConfig } from '$lib/types/site'
+
+export const site: SiteConfig = {
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'rayongfruit.nicekun.dev',
+  title: 'Rayong is Amazing!',
+  subtitle: 'เว็บไซต์รวมผลไม้ในเมีองระยอง',
+  lang: 'en-US',
+  description: 'Powered by SvelteKit/Urara',
+  author: {
+    avatar: '/assets/maskable@512.png',
+    name: 'Chanakan at Rayong',
+    status: '🌸',
+    bio: 'โปรเเกรมเมอร์ที่ไม่รู้อะไรเลยกับเขา.'
+  },
+  themeColor: '#3D4451'
+}
